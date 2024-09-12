b14a4380e2931c428b21bbf423033fa0ea973490
zeroturtle <72597224+zeroturtle@users.noreply.github.com>
2024-09-12 13:47:22 +0300

    5.5.1.30

--- result.js
+++ result.js
@@ -74,7 +74,7 @@ function displayRoundDetails() {
 	//		iframe.src="about:blank"
 			iframe.srcdoc = doc.documentElement.outerHTML
 			iframe.style="max-width:80vw;min-width:200px;min-height:100px;border:none;overflow:hidden;"
-			ifame.addEventListener('load', (e) => {
+			iframe.addEventListener('load', (e) => {
 				iframe.style.height = doc.documentElement.outerHTML.scrollHeight+45+"px";
 				iframe.style.width = doc.documentElement.outerHTML.scrollWidth+45+"px";
 				console.log(e);
