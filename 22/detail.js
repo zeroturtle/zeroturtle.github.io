e0955bff60dbcacfe453d542abbff3f9cae8b173
zeroturtle <72597224+zeroturtle@users.noreply.github.com>
2024-10-18 14:07:09 +0300

    6.5.2

---
+++ 22/detail.js
@@ -0,0 +1,10 @@
+﻿// выделяется крайняя строка таблицы, у которой больше двух колонок
+window.addEventListener("load", (event) => {
+	const rows = document.querySelectorAll("tr")
+	Array.from(rows).filter(item => item.cells.length > 2).pop().classList.add('totalrow')
+
+	rows.forEach((row) => {
+		row.cells.length < 2 ? row.classList.add('noborder') : row.classList.add('bordered')
+	});
+
+})
