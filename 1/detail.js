28e2c987f220f284e116b2beb4a3e1340ebc52a7
zeroturtle <72597224+zeroturtle@users.noreply.github.com>
2024-10-20 17:58:35 +0300

    6.5.2.44

--- 1/detail.js
+++ 1/detail.js
@@ -3,7 +3,7 @@
 	// выделяется крайняя строка таблицы, у которой больше двух колонок
 	Array.from(rows).filter(item => item.cells.length > 2).pop().classList.add('totalrow')
 
-	// set tanle zebra-color
+	// set table zebra-color
 	rows.forEach((row, index) => {
 		if ((row.cells.length > 2) && (index % 2 === 0)) {
 			row.classList.add('even-class');
