893c70bea08faf02bee173f664b5ec4397b0c3dc
zeroturtle <72597224+zeroturtle@users.noreply.github.com>
2024-10-20 15:21:36 +0300

    6.5.2.34

--- 1/detail.js
+++ 1/detail.js
@@ -3,8 +3,16 @@ window.addEventListener("load", (event) => {
 	const rows = document.querySelectorAll("tr")
 	Array.from(rows).filter(item => item.cells.length > 2).pop().classList.add('totalrow')
 
+	var odd = false;
 	rows.forEach((row) => {
-		row.cells.length < 2 ? row.classList.add('noborder') : row.classList.add('bordered')
+		if (row.cells.length < 2) {
+		  row.classList.add('noborder') 
+		}
+		else {
+		  row.classList.add('bordered')
+		  odd = !odd;
+		  row.style.backgroundColor = odd ? #f2f2f2 : #ffffff
+		}
 	});
 
 })
