0e50922b2597b98cdb0d4c1485839aed87e526ca
zeroturtle <72597224+zeroturtle@users.noreply.github.com>
2024-10-20 16:43:28 +0300

    6.5.2.40

--- 1/detail.js
+++ 1/detail.js
@@ -4,8 +4,8 @@
 	Array.from(rows).filter(item => item.cells.length > 2).pop().classList.add('totalrow')
 
 	// set tanle zebra-color
-	rows.forEach((row) => {
-		if (row.cells.length > 2) {
+	rows.forEach((row, index) => {
+		if ((row.cells.length > 2) && (index % 2 === 0)) {
 			row.classList.add('even-class');
 		}
 	});
