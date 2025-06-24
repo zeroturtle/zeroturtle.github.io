7bbf475f52f92f7214cd1967a5b9c4e12876236d
zeroturtle <72597224+zeroturtle@users.noreply.github.com>
2025-06-24 10:58:19 +0300

    Delete 2 directory

--- 2/detail.js
+++
@@ -1,4 +0,0 @@
-﻿// выделяется крайняя строка таблицы, у которой больше двух колонок
-window.addEventListener("load", (event) => {
-	Array.from(document.getElementsByTagName('tr')).filter(item => item.cells.length > 2).pop().classList.add('totalrow')
-})
