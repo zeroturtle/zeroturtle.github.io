bc87b05f2167ed2c45c5b809616aa41d86b618c7
zeroturtle <72597224+zeroturtle@users.noreply.github.com>
2025-06-24 10:57:57 +0300

    Add files via upload

---
+++ events/2/detail.js
@@ -0,0 +1,4 @@
+﻿// выделяется крайняя строка таблицы, у которой больше двух колонок
+window.addEventListener("load", (event) => {
+	Array.from(document.getElementsByTagName('tr')).filter(item => item.cells.length > 2).pop().classList.add('totalrow')
+})
