009d04cc44ec587cbd947c1bc6e388f04d0ff65c
zeroturtle <72597224+zeroturtle@users.noreply.github.com>
2025-07-19 12:53:50 +0300

    Add files via upload

--- event.php
+++ event.php
@@ -15,10 +15,9 @@ define('EVENT_TEMPLATE_DIR', EVENTS_DIR.'template/');
 	//$data = json_decode($json, true);    print_r($data);  die();
 
 // отклоняем запрос при неправильном json PHP>=8.3.0
-//if( !json_validate($_POST['JSON']) ) { die('invalid json') };
+//if( !json_validate($_POST['JSON']) ) { die('invalid json') }
 $event = json_decode($_POST['JSON']); 
 if ( json_last_error() != JSON_ERROR_NONE) { die(json_last_error_msg()); }
-
 // реквизиты соревнования
 $COMPETITION_NAME = $event->name;
 $TYPE = $event->type_id;      //это создаваемый тип соревнований
@@ -26,36 +25,43 @@ $DATE_FROM = $event->date_start;
 $DATE_TO = $event->date_end;
 $PLACE = $event->location;
 $RANK = $event->events;
+$THEME = $event->theme;
+
 
 // check POST variables
 $NUMBER = ( isset($_POST["NUMBER"]) || !ctype_xdigit($_POST["NUMBER"])) ? $_POST["NUMBER"] : 0;
 if (!isset($_POST["NUMBER"]) || !ctype_xdigit($_POST["HASH"])) { die ('Incorrect hash'); } 
 $HASH = $_POST["HASH"];
 
+
+//////////////////////////////
 //Licence_Validation
-$query = "SELECT * FROM LICENCE WHERE ACTIVE=true AND LICENCETYPE=1" //Standard
+$query = "SELECT * FROM LICENCE WHERE ACTIVE=true" 
 ." AND NOW() BETWEEN DATESTART AND DATEEND"	//дата в диапазоне срока действия лицензии
-." AND NUMBER='{$NUMBER}'"
-." AND LICENCEHASH = '{$HASH}'"			//сверяем md5hash
-." AND (EVENTTYPES & (1 << {$TYPE})) <> 0";	//входит ли дисциплина в список лицензии 
-$result = mysqli_query($mysqli, $query); 
-$row = mysqli_fetch_assoc($result);
+." AND LICENCETYPE=1"			//Site
+//." AND NUMBER = ?"			//номер лицензии
+//." AND LICENCEHASH = ?"		//сверяем md5hash
+." AND (EVENTTYPES & (1 << ?)) <> 0";	//тип соревнования входит в список дисциплин лицензии
+$stmt = $pdo->prepare($query);
+//$stmt->execute([$NUMBER, $HASH, $TYPE]); 
+$stmt->execute([$TYPE]); 
+$row = $stmt->fetch();
 if ( is_null($row['LICENCE_ID']) ) die('Error licence validation or Incompatible type!');
 $LicID = $row['LICENCE_ID'];
 
-//проверим повторный запрос создания такого же event
-$query="SELECT * FROM COMPETITION WHERE LICENCE_ID = '{$LicID}' AND DESCRIPTION->'$.id' = '{$event->id}'";
-$result = mysqli_query($mysqli, $query);
-$row = mysqli_fetch_row($result);
-if (!is_null($row)) { die('Event already exists!'); }
+//блокируем повторный запрос создания такого же event
+//$query="SELECT * FROM COMPETITION WHERE LICENCE_ID = ? AND DESCRIPTION->'$.id' = ?";            //MySQL
+$query="SELECT * FROM COMPETITION WHERE LICENCE_ID = ? AND JSON_VALUE(DESCRIPTION, '$.id') = ?";  //MariaDB
+$stmt = $pdo->prepare($query);
+$stmt->execute([$LicID, $event->id]); 
+$row = $stmt->fetch();
+if (!isset($row)) { die('Event already exists!'); }
+/////////////////////////////
+
 
 //сохраняем event в БД
-$GLOBALS['_'] = function ( $v ) { return $v; };
-if (!mysqli_query($mysqli,"INSERT INTO COMPETITION (DESCRIPTION,LICENCE_ID) VALUES('{$_POST['JSON']}', {$LicID})")) {
-  die("Errorcode: " . mysqli_errno($mysqli));
-}
-$result = mysqli_query($mysqli, "SELECT LAST_INSERT_ID();"); 
-$row = mysqli_fetch_row($result);
+$stmt= $pdo->prepare("INSERT INTO COMPETITION (DESCRIPTION,LICENCE_ID) VALUES(?,?)")->execute([json_encode($event), 40]);
+$row = $pdo->query("SELECT LAST_INSERT_ID();")->fetch();
 $COMPETITION_ID = $row[0];     // COMPETITION_ID
 $D = EVENTS_DIR.$COMPETITION_ID;
 rrmdir($D);            // на всякий случай удаляем все 
@@ -78,14 +84,21 @@ $smarty->assign("DATE_FROM", $DATE_FROM);
 $smarty->assign("DATE_TO", $DATE_TO);
 $smarty->assign("PLACE", $PLACE);
 $smarty->assign("RANK", $RANK);
-$smarty->assign("BASEURL", "http://{$_SERVER['SERVER_NAME']}/{EVENTS_DIR}{$COMPETITION_ID}/");
+$smarty->assign("BASEURL", "http://{$_SERVER['SERVER_NAME']}/{$EVENTS_DIR}{$COMPETITION_ID}/");
+$smarty->assign("THEME", (isset($THEME) && in_array($THEME,['dark','light'])) ? $THEME : "light");
+$smarty->assign("PAGE_LOGO", 'Title.jpg');
 $output = $smarty->fetch('index.tpl');					//шаблон страницы соревнований
 file_put_contents(EVENTS_DIR.$COMPETITION_ID.'/index.html',$output);	// write to file event в папку COMPETITION_ID
+
+
+//$PAGE_LOGO = isset($_FILES['page_logo']) ? $_FILES['page_logo'] : 'Title.jpg';
+//organizer_logo
+//competition_logo
 
 // вернуть URL папки event'а
 echo "http://{$_SERVER['SERVER_NAME']}/{EVENTS_DIR}{$COMPETITION_ID}/";
 
-mysqli_close($mysqli);
+
 
 /////////////////////////////////////////////////////
 // from https://www.php.net/manual/ru/function.copy.php example
