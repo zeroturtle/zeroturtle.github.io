a5c8632f5167a472b20671026b226b7b26b335e6
zeroturtle <72597224+zeroturtle@users.noreply.github.com>
2025-08-08 08:17:21 +0300

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-  // Start the session
-  session_start();
-  // Destroy the active session, which logs the user out 
-  session_destroy();
-  // Redirect to the welcone page
-  header('Location: about.html');
-  exit;
-?>
