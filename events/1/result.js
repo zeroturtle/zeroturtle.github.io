2f9ec7c325861b6d2698f0ee9e7b6ab32ee574a6
zeroturtle <72597224+zeroturtle@users.noreply.github.com>
2025-08-14 21:34:18 +0300

    Add files via upload

--- events/1/result.js
+++ events/1/result.js
@@ -2,12 +2,15 @@
 // çàãðóæàåì ïðîòîêîë
 function loadEvent(event) {
 	event.preventDefault()
-	fetch(`${baseURL}${Rank}/proto.html`)
+	fetch(`${baseURL}proto.html`)
 	.then(response => response.text())
 	.then((html) => {
 		let doc = new DOMParser().parseFromString(html, "text/html")
-		//document.head.insertAdjacentHTML('beforeend',`<link type="text/css" rel="stylesheet" href="proto.css">`)
-		let resultTable = doc.querySelector('table')
+		let params = new URLSearchParams(document.location.search)
+		let Rank = parseInt(params.get("r"), 10)
+		Rank = isNaN(Rank) ? 0 : Rank
+		let resultTable = doc.querySelectorAll('table')[Rank]
+		let eventDivepool = Rank; //'FS4';
 		resultTable.setAttribute('id','resultTable')
 		// äëÿ êàæäîé ññûëêå íå "0" äîáàâëÿåì âûçîâ detail
 		for (lnk of [].filter.call(resultTable.getElementsByTagName('a'), item =>(item.pathname.split('/').slice(-1))[0] !=0)) { 
@@ -19,8 +22,7 @@ function loadEvent(event) {
 				lnk.addEventListener("click", displayRoundVideo)
 			}
 			else {  // this is a score details
-				if (Number.isFinite(lnk))
-					lnk.addEventListener("click", displayRoundDetails)
+				lnk.addEventListener("click", displayRoundDetails)
 			}
 		}
 
@@ -33,7 +35,7 @@ function loadEvent(event) {
 
 			c.innerHTML.split(/-/).forEach((v) => {
 				let img = document.createElement('img')
-				img.src = Rank+'/divepool/'+'/'+ v +'.png'
+				img.src = 'divepool/'+eventDivepool+'/'+ v +'.png'
 				span.append(img)
 			});
 			c.onmouseover = function(event) {
@@ -63,7 +65,6 @@ function loadEvent(event) {
 				cell.append(img);
 			}
 		}
-
 		scoreSummary.append(resultTable)
 	})
 }
@@ -74,17 +75,26 @@ function displayRoundVideo() {
 	event.preventDefault()
 }
 
-//Team
+//Team Photo
 function displayTeamDetails() {
 	event.preventDefault()
 	let doc = document.implementation.createHTMLDocument('Team Photo')
 	// найти картинку по записи в 
-	fetch( new URL(`${baseURL}${Rank}/team/`+String(this).split('/').pop()+'.html') )
+	fetch( new URL(baseURL+String(this).split('/').pop()+'.html') )
 		.then(response => response.text())
 		.then((html) => {
 			// Convert the HTML string into a document object
 			let doc = new DOMParser().parseFromString(html, "text/html")
-			doc.head.insertAdjacentHTML('beforeend', `<link type="text/css" rel="stylesheet" href="${baseURL}team.css">`)
+			const table = doc.querySelector('table')
+			const rows = table.querySelectorAll("tr")
+			let img = document.createElement("img");
+			img.setAttribute("src", 'Photo/'+rows[2].cells[0].innerHTML) // tr3 = filename
+			img.setAttribute("alt", 'rows[2].cells[0].innerHTML')
+			img.style.maxHeight = '100%'
+			img.style.maxWidth = '100%'
+			rows[3].cells[0].appendChild(img)			// tr4 - place
+			table.deleteRow(2); 					// remove filename row
+//			doc.head.insertAdjacentHTML('beforeend', '<link type="text/css" rel="stylesheet" href="team.css">')
 			frame.srcdoc = doc.documentElement.outerHTML
 			modal.showModal()
 		})
@@ -95,7 +105,7 @@ function displayTeamDetails() {
 function displayRoundDetails() {
 	event.preventDefault()
 	// çàãðóæàåì ñóäåéñêóþ çàïèñêó	
-	fetch( new URL(`${baseURL}${Rank}/detail/`+String(this).split('/').pop()+'.html') )
+	fetch( new URL(baseURL+String(this).split('/').pop()+'.html') )
 		.then(response => response.text())
 		.then((html) => {
 			// Convert the HTML string into a document object
@@ -110,15 +120,10 @@ function displayRoundDetails() {
 		.catch(error => console.error('Error fetching file:', error))
 }
 
-// приведение в соответствие сокращения стран из 3 букв к названиям флагов из 2 букв.
 const code3=['AUS','AUT','AZE','ALB','DZA','AIA','AGO','AND','ATA','ATG','ANT','ARG','ARM','ABW','AFG','BHS','BGD','BRB','BHR','BLR','BLZ','BEL','BEN','BMU','BVT','BGR','BOL','BIH','BWA','BRA','BRN','BFA','BDI','BTN','VUT','VAT','GBR','HUN','VEN','VGB','VIR','ASM','TMP','VNM','GAB','HTI','GUY','GMB','GHA','GLP','GTM','GIN','GNB','DEU','GIB','HND','HKG','GRD','GRL','GRC','GEO','GUM','DNK','COD','DJI','DMA','DOM','EGY','ZMB','ESH','ZWE','ISR','IND','IDN','JOR','IRQ','IRN','IRL','ISL','ESP','ITA','YEM','CPV','KAZ','CYM','KHM','CMR','CAN','QAT','KEN','CYP','KGZ','KIR','CHN','CCK','COL','COM','COG','CRI','CIV','CUB','KWT','COK','LAO','LVA','LSO','LBR','LBN','LBY','LTU','LIE','LUX','MUS','MRT','MDG','MYT','MAC','MKD','MWI','MYS','MLI','MDV','MLT','MAR','MTQ','MHL','MEX','FSM','MOZ','MDA','MCO','MNG','MSR','MMR','NAM','NRU','NPL','NER','NGA','NLD','NIC','NIU','NZL','NCL','NOR','NFK','ARE','OMN','PAK','PLW','?','PAN','PNG','PRY','PER','PCN','POL','PRT','PRI','REU','CXR','RUS','RWA','ROM','SLV','WSM','SMR','STP','SAU','SWZ','SJM','SHN','PRK','MNP','SYC','VCT','SPM','SEN','KNA','LCA','SGP','SYR','SVK','SVN','USA','SLB','SOM','SDN','SUR','SLE','TJK','THA','TWN','TZA','TCA','TGO','TKL','TON','TTO','TUV','TUN','TKM','TUR','UGA','UZB','UKR','WLF','URY','FRO','FJI','PHL','FIN','FLK','FRA','GUF','PYF','HMD','HRV','CAF','TCD','CZE','CHL','CHE','SWE','LKA','ECU','GNQ','ERI','EST','ETH','YUG','ZAF','SGS','KOR','JAM','JPN','ATF','IOT','UMI']
 const code2=['AU','AT','AZ','AL','DZ','AI','AO','AD','AQ','AG','AN','AR','AM','AW','AF','BS','BD','BB','BH','BY','BZ','BE','BJ','BM','BV','BG','BO','BA','BW','BR','BN','BF','BI','BT','VU','VA','GB','HU','VE','VG','VI','AS','TP','VN','GA','HT','GY','GM','GH','GP','GT','GN','GW','DE','GI','HN','HK','GD','GL','GR','GE','GU','DK','CD','DJ','DM','DO','EG','ZM','EH','ZW','IL','IN','ID','JO','IQ','IR','IE','IS','ES','IT','YE','CV','KZ','KY','KH','CM','CA','QA','KE','CY','KG','KI','CN','CC','CO','KM','CG','CR','CI','CU','KW','CK','LA','LV','LS','LR','LB','LY','LT','LI','LU','MU','MR','MG','YT','MO','MK','MW','MY','ML','MV','MT','MA','MQ','MH','MX','FM','MZ','MD','MC','MN','MS','MM','NA','NR','NP','NE','NG','NL','NI','NU','NZ','NC','NO','NF','AE','OM','PK','PW','PS','PA','PG','PY','PE','PN','PL','PT','PR','RE','CX','RU','RW','RO','SV','WS','SM','ST','SA','SZ','SJ','SH','KP','MP','SC','VC','PM','SN','KN','LC','SG','SY','SK','SI','US','SB','SO','SD','SR','SL','TJ','TH','TW','TZ','TC','TG','TK','TO','TT','TV','TN','TM','TR','UG','UZ','UA','WF','UY','FO','FJ','PH','FI','FK','FR','GF','PF','HM','HR','CF','TD','CZ','CL','CH','SE','LK','EC','GQ','ER','EE','ET','YU','ZA','GS','KR','JM','JP','TF','IO','UM']
 const modal = document.querySelector('dialog')
-const frame = modal.querySelector('iframe')
-
-const urlParams = new URLSearchParams(window.location.search)
-Rank = (urlParams.get("r")==null) ? document.getElementsByClassName("nav-link")[0].getAttribute('href').slice(3) : urlParams.get("r") // default параметр r
-
+const frame = modal.querySelector('iframe')  //getElementById('')
 
 function windowOnClick(event) {
 	if (event.target === modal) {
